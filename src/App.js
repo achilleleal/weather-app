7297ae01dc09a970b688f6e8bb925f16bde6755c
achilleleal <achilleleal@gmail.com>
2020-09-23 17:45:14 -0400

    set up api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './sun.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const key = "";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  getWeather = async () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`)
+    .then(res => res.json())
+    .then(console.log)
+    .catch(console.log)
+  }
+
+  render() {
+    const {loaded} = this.state;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          {loaded ? 
+            <h1>Weather</h1>
+            :
+            <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Loading...</h2>
+            <button onClick={this.getWeather}>Get Weather</button>
+            </>
+            
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
