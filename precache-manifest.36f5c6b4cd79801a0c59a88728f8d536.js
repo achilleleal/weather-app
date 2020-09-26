fec80e6a626ce621ef51041261e6e720d8caf160
achilleleal <achilleleal@gmail.com>
2020-09-26 17:53:38 -0400

    Updates

---
+++ precache-manifest.36f5c6b4cd79801a0c59a88728f8d536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3c2d8e88cb30f2149661a1c09d7a65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e621e87ddfc98497af55",
+    "url": "/static/css/main.bbf01592.chunk.css"
+  },
+  {
+    "revision": "b1e610564bb9f396c1a6",
+    "url": "/static/js/2.01e6ea29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01e6ea29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e621e87ddfc98497af55",
+    "url": "/static/js/main.babb4110.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "fdd37e8a746137bf70574fdd64ec0746",
+    "url": "/static/media/sun.fdd37e8a.svg"
+  }
+]);
