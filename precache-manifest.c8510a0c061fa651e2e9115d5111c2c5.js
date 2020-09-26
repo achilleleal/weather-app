823d7a03e98c16ef32b7e98e36a4a8b251457297
achilleleal <achilleleal@gmail.com>
2020-09-26 18:07:53 -0400

    Updates

---
+++ precache-manifest.c8510a0c061fa651e2e9115d5111c2c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f50f9ac7bdab0c044a66c690944f4ab",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f1868d377388e1bf142d",
+    "url": "/weather-app/static/css/main.c5f4f6ea.chunk.css"
+  },
+  {
+    "revision": "997600db0dc9d6a04f39",
+    "url": "/weather-app/static/js/2.f823aad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.f823aad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1868d377388e1bf142d",
+    "url": "/weather-app/static/js/main.1d6ba472.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "f1e772178d7af7d0af14f3332bafc509",
+    "url": "/weather-app/static/media/github-logo.f1e77217.svg"
+  },
+  {
+    "revision": "086fb7ad88e21149b6dabd85d6aec1db",
+    "url": "/weather-app/static/media/rain.086fb7ad.svg"
+  },
+  {
+    "revision": "fdd37e8a746137bf70574fdd64ec0746",
+    "url": "/weather-app/static/media/sun.fdd37e8a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bbac6a5a61d07cec8c0ffbbb952209d.js"
+  "/weather-app/precache-manifest.c8510a0c061fa651e2e9115d5111c2c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
